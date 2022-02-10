32b784526693f8f11acfe5c23e21d40163ddefae
Siyavush <snajmudinov@gmail.com>
2022-02-10 16:17:40 +0300

    task 2

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 "use strict";
- 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const lastSeenMovie  = prompt('Один из последних просмотренных фильмов?','');
-const movieRating = +prompt('На сколько оцените его?','');
-
-personalMovieDB.movies[lastSeenMovie] = movieRating;
+for (let i = 0; i < 2; i++) {
+  const movie = prompt("Один из последних просмотренных фильмов?", ""),
+    rating = +prompt("На сколько оцените его?", "");
+  if (
+    movie != null &&
+    rating != null &&
+    movie != "" &&
+    rating != "" &&
+    movie.length < 50
+  ) {
+    personalMovieDB.movies[movie] = rating;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 31) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
